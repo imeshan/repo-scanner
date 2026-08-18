30e2d297684c689c82c1ad67c2bddea455791765
Eshan Verma <eshanverma@Eshans-MacBook-Air.local>
2026-08-18 02:22:18 -0700

    Add initial scanner

---
+++ scanner.py
@@ -0,0 +1,12 @@
+from openpyxl import load_workbook
+
+workbook = load_workbook("test_input.xlsx")
+sheet = workbook.active
+
+for row in sheet.iter_rows(min_row=2, values_only=True):
+    repo_url, package, required_version = row
+
+    print(f"Repository: {repo_url}")
+    print(f"Package: {package}")
+    print(f"Required version: {required_version}")
+    print("-" * 40)
