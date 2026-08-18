4ed0549068d8625f7038d7f563a1699591616d91
imeshan <50674889+imeshan@users.noreply.github.com>
2026-08-18 03:05:38 -0700

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -5,3 +5,11 @@ input_file = sys.argv[1]
 
 workbook = load_workbook(input_file)
 sheet = workbook.active
+
+for row in sheet.iter_rows(min_row=2, values_only=True):
+    repo_url, package, required_version = row
+
+    print(f"Repository: {repo_url}")
+    print(f"Package: {package}")
+    print(f"Required version: {required_version}")
+    print("-" * 40)
